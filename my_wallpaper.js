688d0c0bc34187d374f1440c60815be0ad1c59bc
AYANO-8 <ayanodesu0995@gmail.com>
2025-07-27 22:31:08 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -21,25 +21,5 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
- // ellipse(100, 100, 150, 150); // phoebe was here
- stroke(0);
- fill(160);
- rectMode(CENTER);
- rect(100, 100, 120, 120);stroke(0, 0, 255);
-  strokeWeight(3);
-  line(60, 50, 130, 50); // 円の後ろに引く線
-
-  // 2. 緑の円2つ（上半分）
-  noStroke();
-  fill(0, 200, 0);
-  ellipse(70, 50, 50, 50);
-  ellipse(130, 50, 50, 50);
-
-  // 4. 線に触れない位置に楕円（Tシャツ色で塗る）
-  fill(255, 250, 205); // 例：whiteっぽい色
-  ellipse(100, 160, 100, 50);
+  ellipse(100, 100, 150, 150); // phoebe was here
 }
-
-
-
-
